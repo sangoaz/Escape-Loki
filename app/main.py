fe2868fb1c3243421ff975f9e8713167ebb3630e
sangoaz <kevin.fruchon@gmail.com>
2026-04-02 18:19:14 +0200

    Ajout des PP dans le chat

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.routes.game import router as game_router
 
@@ -8,6 +9,8 @@ app = FastAPI(
     version="1.0.0",
     description="Backend FastAPI V1 pour un escape game de Pâques en faux chat.",
 )
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 app.include_router(game_router)
 
